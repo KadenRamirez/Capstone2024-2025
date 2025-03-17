5032e652db5bf5bf2be67d39fcce1ede1b8ae922
Kaden R. <kaden2ramirez3@gmail.com>
2025-03-16 19:51:03 -0500

    Added File Uploads
    Uploads file and stores them in an uploads folder. Uploads folder needs to be accessible by the main program it isn't as of now.

--- KSL-web-app/build.gradle.kts
+++ KSL-web-app/build.gradle.kts
@@ -5,11 +5,16 @@ plugins {
 
 repositories {
     mavenCentral()
+    // mavenLocal()
+    // maven("https://jitpack.io")
 }
 
 dependencies {
-    implementation("io.javalin:javalin:5.6.0") // Javalin Web Framework
+    implementation("io.javalin:javalin:6.5.0") // Updated Javalin version
     implementation("org.thymeleaf:thymeleaf:3.1.1.RELEASE") // Thymeleaf for templating
+    implementation("org.slf4j:slf4j-simple:2.0.9") // Logging for Javalin
+    implementation("commons-io:commons-io:2.11.0") // File handling utilities
+    implementation("org.jetbrains.kotlin:kotlin-stdlib") // Kotlin standard library
 }
 
 application {
