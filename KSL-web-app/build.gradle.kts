4716e4e6a45edc4030d7743b74d81749f6f75310
Kaden R. <kaden2ramirez3@gmail.com>
2025-03-20 12:06:59 -0500

    Added KSL Library Dependency

--- KSL-web-app/build.gradle.kts
+++ KSL-web-app/build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
     implementation("org.slf4j:slf4j-simple:2.0.9") // Logging for Javalin
     implementation("commons-io:commons-io:2.11.0") // File handling utilities
     implementation("org.jetbrains.kotlin:kotlin-stdlib") // Kotlin standard library
+    api(group = "io.github.rossetti", name = "KSLCore", version = "R1.2.0")
 }
 
 application {
